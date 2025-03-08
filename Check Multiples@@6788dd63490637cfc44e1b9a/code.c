3f475df6c8c4687a95d59a4b241a89d8aa458973
Rahul072006 <rahul1381.becse24@chitkara.edu.in>
2025-03-08 14:11:36 +0530

    Solution to Check Multiples

--- Check Multiples@@6788dd63490637cfc44e1b9a/code.c
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-
-int main() {
-    int a, b;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    if (b == 0) {
-        printf("Division by zero is not allowed.\n");
-    } else if (a % b == 0) {
-        printf("Yes\n");
-    } else {
-        printf("No\n");
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a%b==0){
+        printf("Yes");
     }
-
+    else("No");
     return 0;
 }
